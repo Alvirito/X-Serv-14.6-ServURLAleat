4c4a9a6aaef28d056896f0e6f0a55d359778f588
alvaro martinez lopez <lopez@zeta19.aulas.gsyc.es>
2015-02-20 13:40:26 +0100

    Terminado

--- aleat.py
+++ aleat.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python
 import webapp
 import random
-class aleat(webapp.webApp):
 
-	def process(self,parsedRequest):
-		"""Process the relevant elements of the request.
 
-		Devuelve el codigo HTTP de la respuesta y una pag HTML.
-		"""
-		num_al = random.randrange(99999) 
-		return("200 OK" ,"<html><body><h1>Hola Mundo!</h1>" +
-                        "<a href='"  + str(num_al)  +"'>Dame otra!</a>"  +
-                        "</body></html>" +
-                        "\r\n")
+  class aleat(webapp.webApp):
 
-if __name__=="__main__":
-	testWeb=aleat("localhost",1234)
+   def process(self, parsedRequest):
+     num_al = random.randrange(99999) 
+     return("200 OK" , "<html><body><h1>Hola Mundo!</h1>" +
+            "<a href='" + str(num_al) + "'>Dame otra!</a>"  +
+            "</body></html>" +
+            "\r\n")
+  if __name__ == "__main__":
+    testWeb=aleat("localhost",1234)
