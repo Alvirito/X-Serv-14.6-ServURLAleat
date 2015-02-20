1d7b5d73b060f5a08f56a067d965b896265b10b9
alvaro martinez lopez <lopez@zeta19.aulas.gsyc.es>
2015-02-20 13:42:22 +0100

    Terminado

--- aleat.py
+++ aleat.py
@@ -3,13 +3,13 @@ import webapp
 import random
 
 
-  class aleat(webapp.webApp):
+class aleat(webapp.webApp):
 
-   def process(self, parsedRequest):
-     num_al = random.randrange(99999) 
-     return("200 OK" , "<html><body><h1>Hola Mundo!</h1>" +
-            "<a href='" + str(num_al) + "'>Dame otra!</a>"  +
-            "</body></html>" +
-            "\r\n")
-  if __name__ == "__main__":
-    testWeb=aleat("localhost",1234)
+ def process(self, parsedRequest):
+   num_al = random.randrange(99999)
+   return("200 OK", "<html><body><h1>Hola Mundo!</h1>" +
+          "<a href='" + str(num_al) + "'>Dame otra!</a>" +
+          "</body></html>" +
+          "\r\n")
+if __name__ == "__main__":
+  testWeb=aleat("localhost",1234)
